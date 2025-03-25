37b213f139a321bd7063e45bb5c813211ba4adf4
João Pedro Pizoli <jcarvalho@sou.unifeob.edu.br>
2025-03-24 21:58:33 -0300

    Adicionando o botão de audio e finalizando as principais caracteristicas visuais da tela de registro de Alimentos

--- alimenta_ai/lib/pages/home.dart
+++ alimenta_ai/lib/pages/home.dart
@@ -1,12 +1,11 @@
 import 'package:alimenta_ai/models/modelo_categoria.dart';
 import 'package:alimenta_ai/models/ver_dietanutri.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:flutter_svg/svg.dart';
 
 class HomePage extends StatefulWidget {
-  HomePage({super.key});
+  const HomePage({super.key});
 
   @override
   State<HomePage> createState() => _HomePageState();
@@ -16,6 +15,26 @@ class _HomePageState extends State<HomePage> {
   List<ModeloCategoria> categorias = [];
   List<ModeloDieta> dietas = [];
 
+  // Variável para controlar o estado de animação do botão de microfone
+  bool isRecording = false;
+  // Controlador de animação para o botão de microfone
+  double micButtonOffset = 0;
+  // FocusNode para controlar o foco do TextField
+  late FocusNode textFieldFocus;
+
+  @override
+  void initState() {
+    super.initState();
+    textFieldFocus = FocusNode();
+    getInitialInfo();
+  }
+
+  @override
+  void dispose() {
+    textFieldFocus.dispose();
+    super.dispose();
+  }
+
   void getInitialInfo() {
     categorias = ModeloCategoria.getCategorias();
     dietas = ModeloDieta.getDietas();
@@ -23,7 +42,6 @@ class _HomePageState extends State<HomePage> {
 
   @override
   Widget build(BuildContext context) {
-    getInitialInfo();
     return Scaffold(
       appBar: appbar(),
       backgroundColor: Colors.white,
@@ -38,45 +56,174 @@ class _HomePageState extends State<HomePage> {
           SizedBox(
             height: 40,
           ),
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Padding(
-                padding: const EdgeInsets.only(left: 20),
-                child: Text(
-                  'Cardápio do(a)\nNutri:',
-                  style: TextStyle(
-                      color: Colors.black,
-                      fontSize: 18,
-                      fontWeight: FontWeight.w600),
-                ),
-              ),
-              SizedBox(
-                height: 15,
-              ),
-              Container(
-                height: 240,
-                child: ListView.separated(
-                  itemBuilder: (context, index) {
-                    return Container(
-                      width: 210,
-                      decoration: BoxDecoration(
-                        color: dietas[index].boxColor,
-                        borderRadius: BorderRadius.circular(20)
-                      ),
-                    );
-                  },
-                  separatorBuilder: (context, index) => SizedBox(
-                    width: 25,
-                  ),
-                  itemCount: dietas.length,
-                  scrollDirection: Axis.horizontal,
-                ),
-              )
-            ],
-          )
+          dietasNutri(),
+          searchIAField()
         ],
       ),
+    );
+  }
+
+  Container searchIAField() {
+    return Container(
+      margin: EdgeInsets.only(top: 40, left: 20, right: 20),
+      decoration: BoxDecoration(boxShadow: [
+        BoxShadow(
+            // ignore: deprecated_member_use
+            color: Color(0xff1D1617).withOpacity(0.11),
+            blurRadius: 40,
+            spreadRadius: 0.0)
+      ]),
+      child: TextField(
+        focusNode: textFieldFocus,
+        enabled: !isRecording, // Desabilita o TextField durante a gravação
+        decoration: InputDecoration(
+            filled: true,
+            fillColor: Colors.white,
+            contentPadding: EdgeInsets.all(15),
+            hintText: 'Fale o que comeu!',
+            hintStyle: TextStyle(color: Color(0xffDDDADA), fontSize: 14),
+            prefixIcon: Padding(
+              padding: const EdgeInsets.all(12),
+              child: Container(
+                child: Listener(
+                  onPointerDown: (event) {
+                    setState(() {
+                      if (textFieldFocus.hasFocus) {
+                        textFieldFocus.unfocus();
+                      }
+                      micButtonOffset = -10;
+                      isRecording = true;
+                      // Aqui pode adicionar código para iniciar a gravação
+                    });
+                  },
+                  onPointerUp: (event) {
+                    setState(() {
+                      micButtonOffset = 0; // Volta à posição original
+                      isRecording = false;
+                      // Aqui pode adicionar código para parar a gravação
+                    });
+                  },
+                  onPointerCancel: (event) {
+                    setState(() {
+                      micButtonOffset = 0;
+                      isRecording = false;
+                      // Aqui pode adicionar código para cancelar a gravação
+                    });
+                  },
+                  child: AnimatedContainer(
+                    duration: Duration(milliseconds: 150),
+                    transform: Matrix4.translationValues(0, micButtonOffset, 0),
+                    child: SvgPicture.asset(
+                      'assets/icons/mic.svg',
+                      color: isRecording
+                          ? Color(0xff92A3FD)
+                          : null, // Mudar cor quando estiver gravando
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            suffixIcon: SizedBox(
+              width: 100,
+              child: IntrinsicHeight(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    VerticalDivider(
+                      color: Colors.black,
+                      indent: 10,
+                      endIndent: 10,
+                      thickness: 0.1,
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(12),
+                      child: SvgPicture.asset('assets/icons/enviar.svg'),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(15),
+                borderSide: BorderSide.none)),
+      ),
+    );
+  }
+
+  Column dietasNutri() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Padding(
+          padding: const EdgeInsets.only(left: 20),
+          child: Text(
+            'Cardápio do(a)\nNutri:',
+            style: TextStyle(
+                color: Colors.black, fontSize: 18, fontWeight: FontWeight.w600),
+          ),
+        ),
+        SizedBox(
+          height: 15,
+        ),
+        SizedBox(
+          height: 240,
+          child: ListView.separated(
+            itemBuilder: (context, index) {
+              return Container(
+                width: 210,
+                decoration: BoxDecoration(
+                    // ignore: deprecated_member_use
+                    color: dietas[index].boxColor.withOpacity(0.3),
+                    borderRadius: BorderRadius.circular(20)),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    SvgPicture.asset(dietas[index].iconPath),
+                    Text(
+                      dietas[index].name,
+                      style: TextStyle(
+                        fontWeight: FontWeight.w500,
+                        color: Colors.black,
+                        fontSize: 16,
+                      ),
+                    ),
+                    Text(
+                      '${dietas[index].duracao} | ${dietas[index].calorias}',
+                      style: TextStyle(
+                          color: Color(0xff7B6F72),
+                          fontSize: 13,
+                          fontWeight: FontWeight.w400),
+                    ),
+                    Container(
+                      height: 45,
+                      width: 130,
+                      decoration: BoxDecoration(
+                          gradient: LinearGradient(
+                              colors: [Color(0xff9DCEFF), Color(0xff92A3FD)]),
+                          borderRadius: BorderRadius.circular(50)),
+                      child: Center(
+                        child: Text(
+                          'Ver',
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.w600,
+                              fontSize: 14),
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              );
+            },
+            separatorBuilder: (context, index) => SizedBox(
+              width: 25,
+            ),
+            itemCount: dietas.length,
+            scrollDirection: Axis.horizontal,
+            padding: EdgeInsets.only(left: 20, right: 20),
+          ),
+        )
+      ],
     );
   }
 
@@ -95,7 +242,7 @@ class _HomePageState extends State<HomePage> {
         SizedBox(
           height: 15,
         ),
-        Container(
+        SizedBox(
           height: 120,
           child: ListView.separated(
             itemCount: categorias.length,
@@ -108,6 +255,7 @@ class _HomePageState extends State<HomePage> {
               return Container(
                 width: 100,
                 decoration: BoxDecoration(
+                    // ignore: deprecated_member_use
                     color: categorias[index].boxColor.withOpacity(0.3),
                     borderRadius: BorderRadius.circular(16)),
                 child: Column(
@@ -145,11 +293,13 @@ class _HomePageState extends State<HomePage> {
       margin: EdgeInsets.only(top: 40, left: 20, right: 20),
       decoration: BoxDecoration(boxShadow: [
         BoxShadow(
+            // ignore: deprecated_member_use
             color: Color(0xff1D1617).withOpacity(0.11),
             blurRadius: 40,
             spreadRadius: 0.0)
       ]),
       child: TextField(
+        focusNode: textFieldFocus,
         decoration: InputDecoration(
             filled: true,
             fillColor: Colors.white,
@@ -160,7 +310,7 @@ class _HomePageState extends State<HomePage> {
               padding: const EdgeInsets.all(12),
               child: SvgPicture.asset('assets/icons/Search.svg'),
             ),
-            suffixIcon: Container(
+            suffixIcon: SizedBox(
               width: 100,
               child: IntrinsicHeight(
                 child: Row(
@@ -202,14 +352,14 @@ class _HomePageState extends State<HomePage> {
         child: Container(
           margin: EdgeInsets.all(10),
           alignment: Alignment.center,
+          decoration: BoxDecoration(
+              color: Color(0xffF7F8F8),
+              borderRadius: BorderRadius.circular(10)),
           child: SvgPicture.asset(
             'assets/icons/seta_esquerda.svg',
             height: 20,
             width: 20,
           ),
-          decoration: BoxDecoration(
-              color: Color(0xffF7F8F8),
-              borderRadius: BorderRadius.circular(10)),
         ),
       ),
       actions: [
@@ -219,14 +369,14 @@ class _HomePageState extends State<HomePage> {
             margin: EdgeInsets.all(10),
             alignment: Alignment.center,
             width: 37,
+            decoration: BoxDecoration(
+                color: Color(0xffF7F8F8),
+                borderRadius: BorderRadius.circular(10)),
             child: SvgPicture.asset(
               'assets/icons/dots.svg',
               height: 5,
               width: 5,
             ),
-            decoration: BoxDecoration(
-                color: Color(0xffF7F8F8),
-                borderRadius: BorderRadius.circular(10)),
           ),
         )
       ],
