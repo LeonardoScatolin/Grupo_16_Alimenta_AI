64ab524c2eb3a42692a38622d21415d1b6318b08
WesOliveira27 <wesley.oliveira@sou.unifeob.edu.br>
2025-03-24 23:27:07 -0300

    Atualizações no projeto: tela personalizada, ajustes no botão e imagem

--- alimenta_ai/lib/main.dart
+++ alimenta_ai/lib/main.dart
@@ -1,4 +1,3 @@
-import 'package:alimenta_ai/pages/home.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -8,14 +7,79 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData(fontFamily: 'Poppins'),
-      home: HomePage(),
+      home: WelcomeScreen(),
     );
   }
 }
 
+class WelcomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          const Spacer(flex: 2),
+          
+          Center(
+            child: Column(
+              children: [
+                Image.asset(
+                  'assets/images/logo.png',
+                  width: 200,
+                  height: 200,
+                ),
+              ],
+            ),
+          ),
+          const Spacer(flex: 2),
+          
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            child: GestureDetector(
+              onTap: () {
+                
+                print('Botão Começar clicado!');
+              },
+              child: Container(
+                width: 200,
+                height: 40, 
+                decoration: BoxDecoration(
+                  gradient: const LinearGradient(
+                    colors: [
+                      Color.fromARGB(255, 77, 132, 233),
+                      Color.fromARGB(255, 118, 97, 236)
+                    ], 
+                    begin: Alignment.centerLeft,
+                    end: Alignment.centerRight,
+                  ),
+                  borderRadius:
+                      BorderRadius.circular(20), 
+                ),
+                child: const Center(
+                  
+                  child: Text(
+                    'Começar!',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ),
+          const SizedBox(height: 40),
+        ],
+      ),
+    );
+  }
+}
